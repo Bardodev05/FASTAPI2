34e3c337dd58e79d22a8249b9880f5b6eabd281d
Bardodev05 <andersondev0526@gmail.com>
2024-05-14 10:53:10 -0500

    base de datos

---
+++ db/client.py
@@ -0,0 +1,3 @@
+from pymongo import mongo_client
+
+db_client = mongo_client()
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Se importa la clase FastAPI del módulo fastapi
 from fastapi import FastAPI
-from rou import users,product,basic_auth_users,jwt_auth_users
+from rou import users,product,basic_auth_users,jwt_auth_users,users_db
 from fastapi.staticfiles import StaticFiles
 
 # Se crea una nueva instancia de la aplicación FastAPI
@@ -12,6 +12,9 @@ app.include_router(users.router)
 
 app.include_router(basic_auth_users.router)
 app.include_router(jwt_auth_users.router)
+
+app.include_router(users_db.router)
+
 
 app.mount("/static", StaticFiles(directory="static"),name="static")
 
